664fb9b3ad3de0e71f1e32a4e2b700785bd4928c
ayoub.korkot <ayoub.korkot@gmail.com>
2017-01-03 22:16:49 +0000

    modif4

---
+++ js/script2.js
@@ -0,0 +1,35 @@
+
+
+//boucler sur les nombres
+var nbrBtn = document.getElementsByClassName("nbrBtn");
+
+for (i=0;i<nbrBtn.length;i++) {
+	var num = nbrBtn[i];
+	var nbrClick = this.value;
+
+	num.onclick = function () {
+		
+		
+	}
+};
+
+
+//addition
+document.getElementById("addition").onclick = function (){
+
+}
+
+//soustraction
+document.getElementById("soustraction").onclick = function (){
+	
+}
+
+//multiplication
+document.getElementById("multiplication").onclick = function (){
+	
+}
+
+//division
+document.getElementById("division").onclick = function (){
+	
+}
