6467a250911380c31d7504eaae1d5e33d804ab32
aymen korkot <ay.korkot@feeling-group.com>
2017-01-17 15:52:18 +0000

    modif6

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,9 @@
 
 var statusEnCours = "";
-var opEnCours = "";
-
-
-var resultat = 0;
-var resultatMulti = 1;
-var resultatDiv = 1;
-var resultatCalulated = 0;
-var derNbr;
-var conditionDernNbr = 0;
-
-
+var lastNum;
+var cpt =0;
 var btns = document.getElementsByClassName("nbrBtn");
+
 
 // boucler sur les boutons
 for(var i = 0; i < btns.length; i++) {
@@ -20,107 +12,74 @@ for(var i = 0; i < btns.length; i++) {
     btn.onclick = function() {
 
     	var x = this.value;
-
-		if(opEnCours == "") {
-			document.getElementById("resultat").value = document.getElementById("resultat").value + x ;
-		}else{ 
-			
-			document.getElementById("resultat").value = x ;
-			opEnCours == "";
-			
-		}
+    	lastNum = x;
+		document.getElementById("resultat").value = document.getElementById("resultat").value + x ;
+	
     }
 }
  
 
 //addition 
 document.getElementById("addition").onclick = function (){
-	opEnCours = "+";
 	statusEnCours = "+";
-
-	resultat = parseInt(document.getElementById("resultat").value) + resultat;
-	
-	
-	var opValue = this.value;
-	document.getElementById("opEnCours").innerHTML = opValue;	
+	var x = document.getElementById("addition").value;
+	document.getElementById("resultat").value = document.getElementById("resultat").value + " " + x + " ";	
 
 }
 
 //soustraction
 document.getElementById("soustraction").onclick = function (){
-	opEnCours = "-";
 	statusEnCours = "-";
-	resultat = parseInt(document.getElementById("resultat").value) - resultat;	
-	
-	var opValue = this.value;
-	document.getElementById("opEnCours").innerHTML = opValue;	
+	var x = document.getElementById("soustraction").value;
+	document.getElementById("resultat").value = document.getElementById("resultat").value + " " + x + " ";
 }
 
 //multiplication
 document.getElementById("multiplication").onclick = function (){
-	opEnCours = "x";
 	statusEnCours = "x";
-	resultatMulti = parseInt(document.getElementById("resultat").value) * resultatMulti;
-	
-	var opValue = this.value;
-	document.getElementById("opEnCours").innerHTML = opValue;	
+	var x = document.getElementById("multiplication").value;
+	document.getElementById("resultat").value = document.getElementById("resultat").value + " " + x + " ";	
 }
-
 
 
 
 //division
 document.getElementById("division").onclick = function (){
-	opEnCours = "/";
 	statusEnCours = "/";
-	resultatDiv = parseInt(document.getElementById("resultat").value) / resultatDiv;
-	
-	var opValue = this.value;
-	document.getElementById("opEnCours").innerHTML = opValue;	
-}
+	var x = document.getElementById("division").value;
+	document.getElementById("resultat").value = document.getElementById("resultat").value + " " + x + " ";
 
+}
 
 
 
 //fonction resultat final
 document.getElementById("egal").onclick = function (){
 
-	
-	if(conditionDernNbr == 0) {
-		derNbr = parseInt(document.getElementById("resultat").value);
-		conditionDernNbr = 1;
-	}
-	
-	if(statusEnCours=="+"){		
-		resultat = derNbr + resultat;
-		document.getElementById("resultat").value = resultat;
-	}
-	
-	 
-	if(statusEnCours=="x"){
-		resultatMulti = derNbr * resultatMulti;
-		document.getElementById("resultat").value = resultatMulti;
-	}
-	if(statusEnCours=="-"){
-		resultat =resultat;
-		document.getElementById("resultat").value = resultat - derNbr;
-	}
-	if(statusEnCours=="/"){
-		var nbrDiv = derNbr;
-		
-		if(nbrDiv == 0){
-			alert("impossible de diviser sur 0");
-			resetVar();
-		}else{
-			document.getElementById("resultat").value = resultatDiv / nbrDiv;
-		}
-		
-	}
-	
-	document.getElementById("opEnCours").innerHTML = "";
-	resultatCalulated = 1;
-	 
+	var valeur = document.getElementById("resultat").value;
 
+	if(cpt===0){
+		document.getElementById("resultat").value = eval(valeur);
+		cpt = 1;
+	}else{
+		if(statusEnCours==="+"){
+			document.getElementById("resultat").value = eval(valeur+"+"+lastNum);
+		}
+		if(statusEnCours==="-"){
+			document.getElementById("resultat").value = eval(valeur+"-"+lastNum);
+		}
+		if(statusEnCours==="x"){
+			document.getElementById("resultat").value = eval(valeur+"*"+lastNum);
+		}
+		if(statusEnCours==="/"){
+			document.getElementById("resultat").value = eval(valeur+"/"+lastNum);
+		}
+	}
+	
+/*	console.log(lastNum);
+	console.log(valeur);
+	console.log(valeur+"+"+lastNum);*/
+	
 }
 
 
@@ -128,7 +87,6 @@ document.getElementById("egal").onclick = function (){
 // vider le champ
 document.getElementById("cBtn").onclick = function (){
 	document.getElementById("resultat").value = "";
-	resetVar();
 }
 
 
@@ -142,22 +100,8 @@ function effacer(){
 }
 
 
-function resetVar(){
-	opEnCours = "";
-	resultat = 0;
-	resultatCalulated = 0;
-	resultatMulti = 1;
-	conditionDernNbr = 0;
-	document.getElementById("opEnCours").innerHTML = "";
-}
-
-
 function virgule(){
 	var x = document.getElementById("virgule").value;
 	var resultat = document.getElementById("resultat").value;
 	document.getElementById("resultat").value = resultat + x;
 }
-
-
-
-
